db8cea477219515cf87562dd5d8c8300636e5305
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-10-13 09:03:12 +0300

    added upload files feature

--- carchive_proj/carchive_app/urls.py
+++ carchive_proj/carchive_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.showroom_login),
@@ -7,6 +9,13 @@ urlpatterns=[
     path('logout/',views.showroom_logout),
     path('dashboard/',views.cars_dashboard),
     path('add_new_car/',views.add_new_car),
+    path('create_new_car/',views.create_car),
     path('edit_car/<id>/',views.edit_car),
+    path('update_car/<id>/',views.update_car),
     path('show_car/<id>/',views.show_car),
+    path('upload_doc/<id>/',views.upload_doc),
 ] 
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- carchive_proj/carchive_app/views.py
+++ carchive_proj/carchive_app/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render, redirect
 from carchive_app.models import *
 from django.contrib import messages
 import bcrypt
+from datetime import datetime
+from django.core.files.storage import FileSystemStorage
+
+
 # Create your views here.
 def showroom_login(request):
     if 'showroom_id' in request.session:
         return redirect('/dashboard/')
     return render(request,'showroom_login.html')
+
+def showroom_logout(request):
+    if 'showroom_id' in request.session:
+        del request.session['showroom_id']
+    return redirect('/')
+
+def showroom_logged_in(request):
+    if not 'showroom_id' in request.session:
+        return False
+    else:
+        return True
 
 def logging(request):
     if request.method == 'POST':
@@ -28,22 +43,11 @@ def logging(request):
     else:
         return ('/')
 
-def showroom_logout(request):
-    if 'showroom_id' in request.session:
-        del request.session['showroom_id']
-    return redirect('/')
-
-def showroom_logged_in(request):
-    if not 'showroom_id' in request.session:
-        return False
-    else:
-        return True
-
 def cars_dashboard(request):
     if not showroom_logged_in(request):
         return redirect('/')
     context={
-        "cars":Car.objects.all()
+        "cars":Showroom.objects.get(id=request.session['showroom_id']).cars.all()
     }
     return render(request,'cars_dashboard.html',context)
 
@@ -56,12 +60,82 @@ def add_new_car(request):
     }
     return render(request,'add_new_car.html',context)
 
+def create_car(request):
+    if not showroom_logged_in(request):
+        return redirect('/')
+    if request.method == 'POST':
+        model=BrandModel.objects.get(id=request.POST['model'])
+        showroom=Showroom.objects.get(id=request.session['showroom_id'])
+        prod_date=request.POST['prod_date']
+        color=request.POST['color']
+        vin=request.POST['vin']
+        Car.objects.create(model=model,showroom=showroom,prod_date=prod_date,color=color,vin=vin)
+        return redirect('/dashboard/')
+    return redirect('/dashboard/')
+
+
 def edit_car(request,id):
     if not showroom_logged_in(request):
         return redirect('/')
-    return render(request,'edit_car.html')
+    car=Car.objects.get(id=id)
+    car_prod_date=datetime.strftime(car.prod_date,'%Y-%m-%d')
+    if not car in Showroom.objects.get(id=request.session['showroom_id']).cars.all():
+        return redirect('/')
+    context={
+        'car':car,
+        'brands':Brand.objects.all(),
+        'models':BrandModel.objects.all(),
+        'car_prod_date':car_prod_date,
+    }
 
+    return render(request,'edit_car.html',context)
+
+def update_car(request,id):
+    if not showroom_logged_in(request):
+        return redirect('/')
+    if request.method == 'POST':
+        try:
+            car=Car.objects.get(id=id)
+        except:
+            return redirect('/')
+        else:
+            model=BrandModel.objects.get(id=request.POST['model'])
+            prod_date=request.POST['prod_date']
+            color=request.POST['color']
+            vin=request.POST['vin']
+            car.model=model
+            car.prod_date=prod_date
+            car.color=color
+            car.vin=vin
+            car.save()
+            return redirect('/show_car/'+str(car.id)+'/')
+    return ('/')
+    
 def show_car(request,id):
     if not showroom_logged_in(request):
         return redirect('/')
-    return render(request,'show_car.html')
+    car=Car.objects.get(id=id)
+    if not car in Showroom.objects.get(id=request.session['showroom_id']).cars.all():
+        return redirect('/')
+    context={
+        'car':car,
+        'doc_types':DocumentType.objects.all(),
+    }
+    return render(request,'show_car.html',context)
+
+
+def upload_doc(request,id):
+    if request.method == 'POST':
+        uploaded_file=request.FILES['document']
+        doc_type_id = request.POST['doc_type']
+        doc_type=DocumentType.objects.get(id = doc_type_id)
+        car=Car.objects.get(id=id)
+        # print(uploaded_file.name)
+        # print(uploaded_file.size)
+        # fs=FileSystemStorage()
+        # fs.save(uploaded_file.name,uploaded_file)
+        Document.objects.create(car=car,type=doc_type,doc=uploaded_file)
+
+
+
+    return redirect('/show_car/'+str(id)+'/')
